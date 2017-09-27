3e5df4ae243621bdba4e39397f81f7de3f3d8deb
Clint Nicholson <clintondnicholson@gmail.com>
2017-09-26 21:36:43 -0500

    assignment 8 dom scripting: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,25 @@
+var animateLogo = function() {
+  var logo = document.getElementsByClassName('logo');
+  var revealLogo = function() {
+     logo[0].style.opacity = 1;
+     logo[0].style.transform = "scaleX(1) translateY(0)";
+     logo[0].style.msTransform = "scaleX(1) translateY(0)";
+     logo[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+  revealLogo();
+};
+
+
 var animatePoints = function() {
-
-       var points = document.getElementsByClassName('point');
-
-       var revealFirstPoint = function() {
-           points[0].style.opacity = 1;
-           points[0].style.transform = "scaleX(1) translateY(0)";
-           points[0].style.msTransform = "scaleX(1) translateY(0)";
-           points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-       };
-
-       var revealSecondPoint = function() {
-           points[1].style.opacity = 1;
-           points[1].style.transform = "scaleX(1) translateY(0)";
-           points[1].style.msTransform = "scaleX(1) translateY(0)";
-           points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-       };
-
-       var revealThirdPoint = function() {
-           points[2].style.opacity = 1;
-           points[2].style.transform = "scaleX(1) translateY(0)";
-           points[2].style.msTransform = "scaleX(1) translateY(0)";
-           points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-       };
-
-       revealFirstPoint();
-       revealSecondPoint();
-       revealThirdPoint();
-
+  var points = document.getElementsByClassName('point');
+  var revealPoints = function(foo) {
+     points[foo].style.opacity = 1;
+     points[foo].style.transform = "scaleX(1) translateY(0)";
+     points[foo].style.msTransform = "scaleX(1) translateY(0)";
+     points[foo].style.WebkitTransform = "scaleX(1) translateY(0)";
    };
+
+for (v=0; v < points.length; v++) {
+   revealPoints(v);
+   };
+};
