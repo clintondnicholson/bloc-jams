e1f3dce630447a80ed1ac8671d11a8ee994a4c56
Clint Nicholson <clintondnicholson@gmail.com>
2017-10-15 13:51:19 -0500

    assignment-13-dom-scripting-play/pause-part2:completed

--- scripts/album.js
+++ scripts/album.js
@@ -84,8 +84,14 @@ var findParentByClassName = function(element, targetClass) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
-    }
-};
+
+        if (currentParent !== null) {
+        console.log('No parent found.')
+      } else if (currentParent.className !== null) {
+        console.log('No parent found with that class name')
+        }
+      }
+    };
 
 var getSongItem = function(element) {
     switch (element.className) {
@@ -167,30 +173,3 @@ albumImage.addEventListener("click",function(event) {
     }
   });
 };
-
-songListContainer.addEventListener('mouseover', function(event) {
-  if (event.target.parentElement.className === 'album-view-song-item') {
-    var songItem = getSongItem(event.target);
-
-    if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-       songItem.innerHTML = playButtonTemplate;
-     }
-    }
- });
-
- for (var i = 0; i < songRows.length; i++) {
-          songRows[i].addEventListener('mouseleave', function(event) {
-            //#1
-            var songItem = getSongItem(event.target);
-            var songItemNumber = songItem.getAttribute('data-song-number');
-
-            //#2
-            if (songItemNumber !== currentlyPlayingSong) {
-              songItem.innerHTML = songItemNumber;
-            }
-          });
-
-          songRows[i].addEventListener('click', function(event) {
-           clickHandler(event.target);
-          });
-        }
